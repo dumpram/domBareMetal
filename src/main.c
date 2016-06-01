1f6688e1e1029f63c2599cb6b7d307ab0652c19b
Ivan Pavic <ivan.pavic2@fer.hr>
2016-06-01 11:26:33 +0200

    Added GPIO write

--- src/main.c
+++ src/main.c
@@ -1,15 +1,17 @@
 #include <hypercall.h>
 
-#define GPIOX_BASE_ADDR 0x13400000
+#define GPIOX_BASE_ADDR ((unsigned int *)0x41400000)
 #define GPIOX_OFFS_ADDR 0x0C20
 
 
 #define SYSTEM_UP_MSG "domB: Entered main function...\n"
 #define SYSTEM_UP_LEN 32
 
+#define GPIO_INFO_LEN 30
+
 int main () {
 
-//    unsigned int *gpio_base = GPIOX_BASE_ADDR;
+    volatile unsigned int *gpio = GPIOX_BASE_ADDR;
 
     HYPERVISOR_console_io (HYPERCALL_WRITE, SYSTEM_UP_LEN, SYSTEM_UP_MSG);
 
@@ -18,8 +20,13 @@ int main () {
     /** We made it to main **/
     __asm("mov r10, #100");
 
+    // Set direction of GPX1.2 configuration register as out.
+    *(gpio + (0x0c20 >> 2)) |= (0x1 << 8);
+
     while (1367) {
         /** We are stuck in for loop **/
         __asm("mov r9, #100");
+        // GPX1.2 High
+        *(gpio + (0x0c24 >> 2)) |= (1 << 2);
     }
 }
--- src/main.s
+++ src/main.s
@@ -20,25 +20,45 @@
 	.type	main, %function
 main:
 	@ Function supports interworking.
-	@ args = 0, pretend = 0, frame = 0
+	@ args = 0, pretend = 0, frame = 8
 	@ frame_needed = 1, uses_anonymous_args = 0
 	stmfd	sp!, {fp, lr}
 	add	fp, sp, #4
-	ldr	r2, .L3
+	sub	sp, sp, #8
+	ldr	r3, .L3
+	str	r3, [fp, #-8]
+	ldr	r2, .L3+4
 	mov	r1, #32
 	mov	r0, #0
 	bl	HYPERVISOR_console_io
-@ 19 "src/main.c" 1
+@ 21 "src/main.c" 1
 	mov r10, #100
 @ 0 "" 2
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #3104
+	ldr	r2, [fp, #-8]
+	add	r2, r2, #3104
+	ldr	r2, [r2]
+	orr	r2, r2, #256
+	str	r2, [r3]
 .L2:
-@ 23 "src/main.c" 1
+@ 28 "src/main.c" 1
 	mov r9, #100
 @ 0 "" 2
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #3104
+	add	r3, r3, #4
+	ldr	r2, [fp, #-8]
+	add	r2, r2, #3104
+	add	r2, r2, #4
+	ldr	r2, [r2]
+	orr	r2, r2, #4
+	str	r2, [r3]
 	b	.L2
 .L4:
 	.align	2
 .L3:
+	.word	1094713344
 	.word	.LC0
 	.size	main, .-main
 	.ident	"GCC: (crosstool-NG linaro-1.13.1-4.9-2014.09 - Linaro GCC 4.9-2014.09) 4.9.2 20140904 (prerelease)"
